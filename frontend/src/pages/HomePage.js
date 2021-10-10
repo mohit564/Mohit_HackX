0ff9cbf86f192897ef2a57478dd366c188d5e720
Mohit Dhule <86644125+mohit564@users.noreply.github.com>
2021-10-10 09:05:27 +0530

    style: fix contrast and image alignment

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -26,19 +26,19 @@ const HomePage = () => {
   }
 
   return (
-    <main className="min-h-screen text-white bg-gray-900 ">
+    <main className="min-h-screen text-black bg-gray-100 ">
       <section className="container px-6 py-6 mx-auto">
-        <h2 className="mb-10 text-2xl text-center">TV Channels</h2>
-        <div className="grid grid-cols-1 gap-5 lg:grid-cols-4 md:grid-cols-3">
+        <h2 className="mb-10 text-2xl font-medium text-center">TV Channels</h2>
+        <div className="grid grid-cols-1 gap-5 auto-rows-fr lg:grid-cols-4 md:grid-cols-3">
           {channels.map((channel) => {
             return (
               <Link
                 key={channel._id}
                 to={`channels/${channel._id}`}
-                className="rounded hover:bg-gray-800"
+                className="p-5 rounded hover:bg-gray-200 max-h-40"
               >
                 <img
-                  className="object-cover object-center mx-auto"
+                  className="object-contain object-center h-full mx-auto align-middle"
                   src={channel.image}
                   alt={"Channel" + channel.name}
                   loading="lazy"
